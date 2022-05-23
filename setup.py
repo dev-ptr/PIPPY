ed6a7ad4e0ce173416e0dc9b790534e5a652093e
Shannon Huggins <phate@houseofsyn.com>
2022-05-24 06:52:35 +1000

    - Added function for exec loop to make it cleaner - Added function to catch replace exceptions to clean up the code

--- setup.py
+++ setup.py
@@ -20,118 +20,53 @@ def replace_num(file,initial,new_num):
     with open(file,"w") as f:
         f.writelines(newline)
 
-for x in range(1,4):
-	if os.system("sudo apt update") == 0:
-		break
 
-for x in range(1,4):
-	if os.system("sudo apt -y dist-upgrade") == 0:
-		break
+def exec_loop(command):
+	for x in range(1,4):
+		if os.system(command) == 0:
+			break
 
-for x in range(1,4):
-	if os.system("sudo apt clean") == 0:
-		break
+def catch_replace_exception(file, string, replacement):
+	try:
+		replace_num(file, string, replacement)
+	except:
+		print("try again")
 
-for x in range(1,4):
-	if os.system("sudo pip3 install -U pip") == 0:
-		break
+# Rather than repeat code 100* we'll actually use a function for it
+exec_loop("sudo apt update")
+exec_loop("sudo apt -y dist-upgrade")
+exec_loop("sudo apt clean")
+exec_loop("sudo pip3 install -U pip")
+exec_loop("sudo apt-get install -y python-dev python3-pip libfreetype6-dev libjpeg-dev build-essential")
+exec_loop("sudo -H pip3 install --upgrade luma.oled")
+exec_loop("sudo apt-get install -y i2c-tools")
+exec_loop("sudo apt-get install -y python3-smbus")
 
-for x in range(1,4):
-	if os.system("sudo apt-get install -y python-dev python3-pip libfreetype6-dev libjpeg-dev build-essential") == 0:
-		break
+# Now we install all the things that were left out... (In the event we aren't using a bloated GUI image)
+exec_loop("sudo apt-get install -y libharfbuzz0 libavcodec58 libavformat58 libswscale5 libgtk-3-0 libilmbase-dev libopenexr-dev")
+exec_loop("sudo pip3 install icm20948")
+exec_loop("sudo pip3 install flask")
+exec_loop("sudo pip3 install flask_cors")
+exec_loop("sudo pip3 install websockets")
 
-for x in range(1,4):
-	if os.system("sudo -H pip3 install --upgrade luma.oled") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get install -y i2c-tools") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get install -y python3-smbus") == 0:
-		break
-
-# Now we install all the things that were left out...
-for x in range(1,4):
-	if os.system("sudo apt-get install -y libharfbuzz0 libavcodec58 libavformat58 libswscale5 libgtk-3-0 libilmbase-dev libopenexr-dev") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install icm20948") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install flask") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install flask_cors") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install websockets") == 0:
-		break
-
-try:
-	replace_num("/boot/config.txt",'#dtparam=i2c_arm=on','dtparam=i2c_arm=on')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'[all]','[all]\ngpu_mem=128')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1\nstart_x=1')
-except:
-	print('try again')
-
-try:
-	replace_num("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1')
-except:
-	print('try again')
+# like our consolidation of exec loops, we'll do the same for exception
+catch_replace_exception("/boot/config.txt",'#dtparam=i2c_arm=on','dtparam=i2c_arm=on')
+catch_replace_exception("/boot/config.txt",'[all]','[all]\ngpu_mem=128')
+catch_replace_exception("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1\nstart_x=1')
+catch_replace_exception("/boot/config.txt",'camera_auto_detect=1','#camera_auto_detect=1')
 
 
-for x in range(1,4):
-	if os.system("sudo pip3 install opencv-contrib-python==3.4.11.45") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple opencv-contrib-python==3.4.11.45") == 0:
-		break
+exec_loop("sudo pip3 install opencv-contrib-python==3.4.11.45")
 
-for x in range(1,4):
-	if os.system("sudo pip3 uninstall -y numpy") == 0:
-		break
 
-for x in range(1,4):
-	if os.system("sudo pip3 install numpy==1.21") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple numpy==1.21") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install imutils zmq pybase64 psutil") == 0:
-		break
-	elif os.system("sudo pip3 install -i http://pypi.douban.com/simple/ --trusted-host=pypi.douban.com/simple imutils zmq pybase64 psutil") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq") == 0:
-		break
-
-for x in range(1,4):
-	if os.system("sudo pip3 install pi-ina219") == 0:
-		break
-
-        
-for x in range(1,4):
-	if os.system("cd " + thisPath + " && cd .. && sudo git clone https://github.com/oblique/create_ap") == 0:
-		break
+# Removed the fallback because it potentially either doesn't hit or if it does can potentially break
+exec_loop("sudo pip3 uninstall -y numpy")
+exec_loop("sudo pip3 install numpy==1.21")
+exec_loop("sudo apt-get -y install libhdf5-dev libhdf5-serial-dev libatlas-base-dev libjasper-dev")
+exec_loop("sudo pip3 install imutils zmq pybase64 psutil")
+exec_loop("sudo apt-get install -y util-linux procps hostapd iproute2 iw haveged dnsmasq")
+exec_loop("sudo pip3 install pi-ina219")
+exec_loop("cd " + thisPath + " && cd .. && sudo git clone https://github.com/oblique/create_ap")
 
 try:
 	os.system("cd " + thisPath + " && cd .. && cd create_ap && sudo make install")
@@ -145,7 +80,7 @@ if run_string not in file.read():
 	replace_num('/etc/rc.local','exit 0', run_string)
 else:
 	print("Start up already in /etc/rc.local, skipping...")
-	
+
 print('Completed!')
 
 os.system("sudo reboot")
